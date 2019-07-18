d2cb06b35bbb3df5cb11d601183dce46652fc36f
KU <ku@digitaldolphins.jp>
2019-07-18 17:44:14 +0900

    0.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("LEML")]
-[assembly: AssemblyDescription("Light EML file parser")]
+[assembly: AssemblyDescription("Light EML RFC822 file reader parser")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HIRAOKA HYPERS TOOLS, Inc.")]
 [assembly: AssemblyProduct("LEML")]
-[assembly: AssemblyCopyright("Copyright (c) 2017")]
+[assembly: AssemblyCopyright("Copyright (c) 2017, 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly:     AssemblyVersion("0.2")]//here
-[assembly: AssemblyFileVersion("0.2")]//here
+[assembly:     AssemblyVersion("0.3")]//here
+[assembly: AssemblyFileVersion("0.3")]//here
