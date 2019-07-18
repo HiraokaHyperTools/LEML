119b99ca813308bca0365a64ec01c45ce74227b2
KU <ku@digitaldolphins.jp>
2019-07-18 17:46:27 +0900

    - nuspec updates.

--- P.bat
+++ P.bat
@@ -1,2 +1,3 @@
-\DL\nuget pack LEML.csproj -Build -Properties Configuration=Release
+@rem https://raw.githubusercontent.com/HiraokaHyperTools/LEML/v0.3/README.md
+nuget pack LEML.csproj -Build -Properties Configuration=Release
 pause
